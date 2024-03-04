0cdf78a373eb976c01d8a2a4bf2237462f6150fc
ImSighted <thomasbowles58@gmail.com>
2024-03-04 12:08:14 +0000

    Die Code

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -14,10 +14,16 @@ namespace CMP1903_A1_2324
          */
 
         //Property
-
-
+        public int die_num { get; set; } // establishes get and set for die_num
+        static Random rnd = new Random(); // makes a new random object for the class, outside 'Roll()' method to allow for different random per roll
         //Method
+        public int Roll()
+        {
+            // creates random
+            int die_num = rnd.Next(1, 7); // generates random number between 1-6
+            return die_num; // returns random the random number between 1-6
 
+        }
 
     }
 }
