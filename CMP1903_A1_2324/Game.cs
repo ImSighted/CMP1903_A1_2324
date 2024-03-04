94af150df7c0f94eb2cad52d5b1571b78da67fb2
ImSighted <thomasbowles58@gmail.com>
2024-03-04 11:54:05 +0000

    Finished Code.

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,8 +17,22 @@ namespace CMP1903_A1_2324
          * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
+      private Die die_1 = new Die(); // Creates 3 die objects
+      private Die die_2 = new Die();
+      private Die die_3 = new Die();
+
+
+
 
         //Methods
+        public int Add_Die_Rolls()
+        {
+            int die_roll1 = die_1.Roll(); // rolls all 3 die objects
+            int die_roll2 = die_2.Roll();
+            int die_roll3 = die_3.Roll();
 
+            int die_total = die_roll1 + die_roll2 + die_roll3; // totals die rolls
+            return die_total; // returns die_total
+        }
     }
 }
--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,32 @@ namespace CMP1903_A1_2324
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+
             /*
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
+
+            Game game = new Game(); // creates game oject
+            Testing testing = new Testing(); // creates test object
+
+
+            Console.WriteLine("Welcome To The Dice Roll Application"); // welcomes user
+            int die_total = game.Add_Die_Rolls(); // calls method
+            Console.WriteLine($"The Total Of The 3 Dice Rolls Are {die_total}"); // prints die total
+            testing.Check_Dice_Number(); // calls testing method
+            Console.ReadLine(); // allows user to quit window by pressing enter
+
+
+
+
+
+
+
+
         }
     }
 }
